b24becbc2d552cb1d8fb28b1dc96feebdf000e1b
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-04-22 09:40:27 +0100

    update pwa name

--- assets/sw.js
+++ assets/sw.js
@@ -1,25 +1,25 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = "particle-simulation-pwa";
 var filesToCache = [
-  './',
-  './index.html',
-  './particle-simulation.js',
-  './particle-simulation_bg.wasm',
+  "./",
+  "./index.html",
+  "./particle-simulation.js",
+  "./particle-simulation_bg.wasm",
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
+self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
       return cache.addAll(filesToCache);
-    })
+    }),
   );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
+self.addEventListener("fetch", function (e) {
   e.respondWith(
     caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
-    })
+    }),
   );
 });
